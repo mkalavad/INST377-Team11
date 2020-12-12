3d4a2c53889699e8ff57da480f79bec1a96e98ca
Jessica Tsai <jtsai123@terpmail.umd.edu>
2020-12-11 21:50:06 -0500

    re-run package.jsons

--- public/script.js
+++ public/script.js
@@ -2,27 +2,27 @@ let songsArray = [];
 let artistsArray = [];
 
 
-function getAsText(fileToRead){
+function getAsText(fileToRead) {
     let reader = new FileReader()
     reader.readAsText(fileToRead);
     reader.onload = loadHandler;
 }
 
-function handleFiles(files){
+function handleFiles(files) {
     if(window.FileReader){
         getAsText(files[0]);
     }
 }
 
-function loadHandler(event){
+function loadHandler(event) {
     let csv = event.target.result;
     processData(csv);
 }
 
-function processData(csv){
+function processData(csv) {
     let allTextLines = csv.split(/\r\n|\n);
 
-    for(let i = 0; i < allTextLines.length; i++){
+    for (let i = 0; i < allTextLines.length; i++) {
         let row = allTextLines[i].split(',');
         let col = [];
         let col2 = [];
@@ -31,22 +31,22 @@ function processData(csv){
             col.push(row[1]);
             col2.push(row[2]);
         }
-    
+
         songsArray.push(col);
         artistsArray.push(col2);
     }
 }
 
 
-var templateSource = document.getElementById('results-template').innterHTML,
-    template = Handlebars.compile(templateSource), 
+let templateSource = document.getElementById('results-template').innterHTML,
+    template = Handlebars.compile(templateSource);
     resultsPlaceholder = document.getElementById('results');
 
 
-function getHits(){
-    for(let j = 0; j < artistsArray.length, j++){
-        var findSongs = function(artistsArray[j]){
-        for(let i = 0; i < songsArray.length, i++){
+function getHits() {
+    for (let j = 0; j < artistsArray.length, j++) {
+        var findSongs = function(artistsArray[j]) {
+        for (let i = 0; i < songsArray.length, i++) {
                 $.ajax({
                     url: 'https://api.spotify.com/v1/search',
                     data: {
