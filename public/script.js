24380e86b9f45a0d0f46ade7b62c9f49a189ac0e
ptrkx <70548323+ptrkx@users.noreply.github.com>
2020-12-14 07:42:20 -0500

    update

--- public/script.js
+++ public/script.js
@@ -60,3 +60,22 @@ function tempsearch(query2) {
     });
   };
 };
+
+
+document.body.addEventListener('submit', async (e) => {
+  e.preventDefault(); // this stops whatever the browser wanted to do itself.
+  const form = $(e.target).serializeArray();
+  fetch('/api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(form)
+  })
+    .then((fromServer) => fromServer.json())
+    .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
