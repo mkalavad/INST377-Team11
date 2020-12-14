b5013167290bea52c2c7c488452c321177bf6287
Jessica Tsai <jtsai123@terpmail.umd.edu>
2020-12-14 15:25:14 -0500

    read CSV and turns into JSON string

--- public/script.js
+++ public/script.js
@@ -1,22 +1,82 @@
-import cvsReader from 'neat-cvs';
-import {promises as fs} from 'fs';
-import path from 'path';
+import { response } from "express";
+// const express = require('express');
 
-const directoryname = path.resolve;
+let ratingArray = [];
+let songsArray = [];
+let artistsArray = [];
 
-function callbackFunction(args, cb) {
-  const newValue = args * args; // rip type protection
-  cb = newValue;
+/****turn csv into a json and clean up code/check for bugs*****/
+
+
+function getAsText(fileToRead) {
+  let reader = new FileReader();
+  reader.readAsText(fileToRead);
+  reader.onload = loadHandler;
 }
 
-callbackFunction(2, (res) => {
-  console.log(res); // it will be 4
-  callbackFunction(res, (what) => {
-    console.log(what); // it will be 16
-    callbackFunction(what, (ohno) => {
-      console.log(ohno); // it will be 256
-    });
-  });
-});
+function handleFiles(files) {
+  if (window.FileReader) {
+    getAsText(files[0]);
+  }
+}
 
-callbackFunction();
+function loadHandler(event) {
+  let csv = event.target.result;
+  processData(csv);
+}
+
+function processData(csv) {
+  let allTextLines = csv.split("/\r\n|\n");
+
+  for (let i = 0; i < allTextLines.length; i++) {
+    let row = allTextLines[i].split(",");
+    let col1 = [];
+    let col = [];
+    let col2 = [];
+
+    for (let j = 0; j < row.length; j++) {
+      col1.push(row[0]);
+      col.push(row[1]);
+      col2.push(row[2]);
+    }
+    ratingArray.push(col1);
+    songsArray.push(col);
+    artistsArray.push(col2);
+  }
+}
+
+
+
+
+let templateSource = document.getElementById("results-template").innerHTML,
+  template = Handlebars.compile(templateSource),
+  resultsPlaceholder = document.getElementById("results");
+
+function getHits() {
+  for (let j = 0; j < artistsArray.length; j++) {
+    tempsearch(artistsArray[j]);
+  }
+}
+
+function tempsearch(query2) {
+  for (let i = 0; i < songsArray.length; i++) {
+    $.ajax({
+      url: "https://api.spotify.com/v1/search",
+      data: {
+        q: songsArray[i],
+        type: "song",
+      },
+      success: function (response) {
+        resultsPlaceholder.innerHTML = template(response);
+      },
+    });
+  };
+};
+
+
+
+for (let k = 0; k < songsArray.length; k++){
+let song_name = songsArray[k];
+let artist = artistsArray[k];
+response.json({"rating": rating, "song_title": song_name, "artist": artist});
+}
